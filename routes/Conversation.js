576832a4712dd43765986d797d90cac750514a87
vishal <vvish4me@gmail.com>
2022-07-22 23:26:30 +0530

    updating conversation.js

--- routes/Conversation.js
+++ routes/Conversation.js
@@ -14,9 +14,11 @@ route.post("/createConversation", FetchUser, async (req, res) => {
 });
 route.get("/:id", FetchUser, async (req, res) => {
   try {
-    const Conversations = await conversation.find({
-      members: { $in: [req.params.id] },
-    });
+    const Conversations = await conversation
+      .find({
+        members: { $in: [req.params.id] },
+      })
+      .lean();
     return res.status(201).json(Conversations);
   } catch (err) {
     return res.status(500).json({ error: err });
@@ -26,6 +28,23 @@ route.get("/getConversation/:id", FetchUser, async (req, res) => {
   try {
     const Conversation = await conversation.findById(req.params.id);
     return res.status(201).json(Conversation);
+  } catch (err) {
+    return res.status(500).json({ error: err });
+  }
+});
+route.get("/getConversationByreceiver/:id", FetchUser, async (req, res) => {
+  try {
+    const { id } = req.params;
+    let Conversation = await conversation
+      .findOne({ members: { $all: [id, req.user.id] } })
+      .lean();
+    if (!Conversation) {
+      Conversation = await conversation.create({
+        members: [id, req.user.id],
+      });
+    }
+    console.log(Conversation)
+    return res.status(200).json(Conversation);
   } catch (err) {
     return res.status(500).json({ error: err });
   }
